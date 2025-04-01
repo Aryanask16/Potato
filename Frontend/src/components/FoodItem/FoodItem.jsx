4a03fa0f3e3b9d2ae6ebddfcb39449606c8edde2
Aryanask16 <166402806+Aryanask16@users.noreply.github.com>
2025-04-01 13:56:25 +0530

    Update FoodItem.jsx

--- Frontend/src/components/FoodItem/FoodItem.jsx
+++ Frontend/src/components/FoodItem/FoodItem.jsx
@@ -2,33 +2,38 @@ import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
-const FoodItem = ({id,name,price,description, image}) => {
-    const{cartItems,addToCart,removeFromCart,url} = useContext(StoreContext)
-  return (
-    <div>
-        <div className='food-item'>
-            <div className="food-item-img-container">
-                <img className='food-item-image' src={url+"/images/"+image} alt="" />
-                {!cartItems[id]
-                 ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" srcset="" />
-                 :<div className='food-item-counter'>
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" srcset="" /> 
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-                 </div> 
-                }
-            </div>
-            <div className="food-item-info">
-                <div className="food-item-name-rating">
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt="" />
+
+const FoodItem = ({id, name, price, description, image, onClick}) => {
+    const{cartItems, addToCart, removeFromCart, url} = useContext(StoreContext)
+    
+    return (
+        <div>
+            <div className='food-item' onClick={onClick}>
+                <div className="food-item-img-container">
+                    <img className='food-item-image' src={url+"/images/"+image} alt="" />
+                    {/* Stop propagation to prevent modal from opening when clicking add/remove buttons */}
+                    <div onClick={(e) => e.stopPropagation()}>
+                        {!cartItems[id]
+                         ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" srcset="" />
+                         :<div className='food-item-counter'>
+                            <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" srcset="" /> 
+                            <p>{cartItems[id]}</p>
+                            <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+                         </div> 
+                        }
+                    </div>
                 </div>
-                <p className="food-item-desc">{description}</p>
-                <p className="food-item-price">${price}</p>
+                <div className="food-item-info">
+                    <div className="food-item-name-rating">
+                        <p>{name}</p>
+                        <img src={assets.rating_starts} alt="" />
+                    </div>
+                    <p className="food-item-desc">{description}</p>
+                    <p className="food-item-price">₹{price}</p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default FoodItem
